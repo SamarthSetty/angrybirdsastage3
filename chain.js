a296613b8509faee79cee1af163d10b45515939e
SamarthSetty <samarth.setty30@gmail.com>
2020-07-09 20:43:39 +0530

    addedConstraint

---
+++ chain.js
@@ -0,0 +1,19 @@
+class Chain {
+ constructor (bodyA,bodyB){
+     
+    var options = {
+        bodyA:bodyA,
+        bodyB:bodyB,
+        stiffness:0.04,
+        length:10
+    }
+    this.Chain=Constraint.create(options);
+    World.add(world,this.Chain);
+ }
+
+display()
+{strokeWeight(3);
+    line(this.Chain.bodyA.position.x,this.Chain.bodyA.position.y,this.Chain.bodyB.position.x,this.Chain.bodyB.position.y);
+
+}
+}
--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chain;
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -23,6 +25,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
+    log6 = new Log(230,180,80,PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -35,6 +38,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+   chain = new Chain(bird.body,log6.body);
 
 }
 
@@ -54,6 +59,7 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
+    log6.display();
 
     box5.display();
     log4.display();
@@ -61,4 +67,6 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    chain.display();
 }
